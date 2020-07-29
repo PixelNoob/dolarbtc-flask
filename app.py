05e51415b71cfabbe2a7b6ad67ba19916958cfbb
chitty <chitty@eosargentina.io>
2020-07-29 00:10:56 +0000

    better data format in get_prices, deprecating .csv

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, jsonify
 import requests as r
 import json
 from flask_bootstrap import Bootstrap
-from flask import Response
 
 def create_app():
   app = Flask(__name__)
@@ -15,30 +14,15 @@ app = create_app()
 @app.route('/')
 @app.route('/home')
 def home():
-    fname = "data.csv"
-    file= open(fname)
-    list= []
-    for line in file:
-        line= line.rstrip()
-        words = line.split()
-        list.append(words[1][1:])
-    return render_template("bootstrap.html", value1 = list[1], value2 = list[2], value3 = list[3], value4 = "{:,.0f}".format(int(list[4])))
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    return render_template("bootstrap.html", value1 = data['compra'], value2 = data['venta'], value3 = data['promedio'], value4 = "{:,.0f}".format(int(data['bitcoin'])))
 
 @app.route('/api')
 def api():
-    fname = "data.csv"
-    file= open(fname)
-    list= []
-    for line in file:
-        line= line.rstrip()
-        words = line.split()
-        list.append(words[1][1:])
-    dict = {}
-    dict.update({'compra':float(list[1])})
-    dict.update({'venta':float(list[2])})
-    dict.update({'promedio':float(list[3])})
-    dict.update({'bitcoin':int(list[4])})    
-    return jsonify(dict)
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8888, debug=False)
--- get_prices.py
+++ get_prices.py
@@ -1,63 +1,45 @@
 import requests as r
 import json
-import time
-import csv
 
-# Function to turn url int json
-def url_json(url):
-    json = r.get(url).json()
-    return json
+def get_ask():
+    lb_ask = r.get("https://localbitcoins.com/buy-bitcoins-online/ars/national-bank-transfer/.json").json()
+    ask_a = float(lb_ask["data"]["ad_list"][1]["data"]["temp_price"])
+    ask_b = float(lb_ask["data"]["ad_list"][2]["data"]["temp_price"])
+    ask_c = float(lb_ask["data"]["ad_list"][3]["data"]["temp_price"])
+    ask_medium = (ask_a + ask_b + ask_c) / 3
+    return round(ask_medium, 2)
 
-# 3 ask prices from lb ads
-lb_ask = "https://localbitcoins.com/buy-bitcoins-online/ars/national-bank-transfer/.json"
-lb_ask = url_json(lb_ask)
+def get_buy():
+    lb_buy = r.get("https://localbitcoins.com/sell-bitcoins-online/ars/national-bank-transfer/.json").json()
+    buy_a = float(lb_buy["data"]["ad_list"][1]["data"]["temp_price"])
+    buy_b = float(lb_buy["data"]["ad_list"][2]["data"]["temp_price"])
+    buy_c = float(lb_buy["data"]["ad_list"][3]["data"]["temp_price"])
+    buy_medium = (buy_a + buy_b + buy_c) / 3
+    return round(buy_medium, 2)
 
-ask_a = float(lb_ask["data"]["ad_list"][1]["data"]["temp_price"])
-ask_b = float(lb_ask["data"]["ad_list"][2]["data"]["temp_price"])
-ask_c = float(lb_ask["data"]["ad_list"][3]["data"]["temp_price"])
+def get_btc():
+    medium = round((get_ask() + get_buy()) / 2)
+    return medium
 
-# 3 buy prices from lb ads
-lb_buy = "https://localbitcoins.com/sell-bitcoins-online/ars/national-bank-transfer/.json"
-lb_buy = url_json(lb_buy)
 
-buy_a = float(lb_buy["data"]["ad_list"][1]["data"]["temp_price"])
-buy_b = float(lb_buy["data"]["ad_list"][2]["data"]["temp_price"])
-buy_c = float(lb_buy["data"]["ad_list"][3]["data"]["temp_price"])
+def get_dolarbtc():
+    btc = r.get("https://blockchain.info/ticker").json()
+    price_btc = float(btc["USD"]["15m"])
+    list = []
+    list.append(round(get_ask()/price_btc,2))
+    list.append(round(get_buy()/price_btc,2))
+    list.append(round(get_btc()/price_btc,2))
+    return list
 
-ask_medium = (ask_a + ask_b + ask_c) / 3
-buy_medium = (buy_a + buy_b + buy_c) / 3
+def create_dict():
+    dict = {}
+    list = get_dolarbtc()
+    dict.update({'compra':list[0]})
+    dict.update({'venta':list[1]})
+    dict.update({'promedio':list[2]})
+    dict.update({'bitcoin':get_btc()})
+    return dict
 
-medium = (ask_medium + buy_medium) / 2
-print ("Precio Bitcoin en Argentina " + "{:,.0f}".format(medium))
-
-# international price of bitcoin in usd from Blockchain.info api
-btc = "https://blockchain.info/ticker"
-btc = url_json(btc)
-
-price_btc = float(btc["USD"]["15m"])
-# print ("El precio internacional del Bitcoin es", price_btc)
-
-# precio del Dolar Bitcoin
-buy_dolar_btc = ask_medium / price_btc
-sell_dolar_btc = buy_medium / price_btc
-dolar_btc = medium / price_btc
-bitcoin = "{:.0f}".format(medium)
-print ("El precio del Dólar Bitcoin en Argentina es...")
-time.sleep(1)
-print ("Compra", "{:.2f}".format(buy_dolar_btc))
-time.sleep(1)
-print ("Venta", "{:.2f}".format(sell_dolar_btc))
-time.sleep(1)
-print ("Promedio", "{:.2f}".format(dolar_btc))
-
-### Put prices on a csv file
-
-csvfile=open('/root/dolarbtc/dolarbtc/data.csv','w', newline='')
-file=csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-file.writerow([time.ctime()])
-file.writerow(["compra ", "{:.2f}".format(buy_dolar_btc)])
-file.writerow(["venta ", "{:.2f}".format(sell_dolar_btc)])
-file.writerow(["promedio ", "{:.2f}".format(dolar_btc)])
-file.writerow(["Bitcoin ", bitcoin])
-
+## write to a json file
+with open('/root/dolarbtc/dolarbtc/data.json', 'w') as f:
+    f.write(json.dumps(create_dict()))
